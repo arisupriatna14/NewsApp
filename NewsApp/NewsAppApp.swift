738a9c73bb1ab3d374b0e4a59b5f22b6fe902871
Ari Supriatna <ari.supriatna@bibit.id>
2021-10-14 23:16:21 +0700

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Ari Supriatna on 14/10/21.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
